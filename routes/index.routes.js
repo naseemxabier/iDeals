d6c864276c07afbcf919e29b88eb6f0116695386
Lu <luciagomezgastaldu@gmail.com>
2023-02-22 17:49:14 +0100

    admin permisos

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User.model");
+const Deal = require("../models/Deal.model");
 
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/",  (req, res, next) => {
+
+  Deal.find()
+    .populate("creator")
+    .then(result => {
+       console.log("resultHOME", result) 
+       /* console.log({result, user: req.session.currentUser}) */
+       res.render("index",{result:result} )
+    } )
 }); 
  
 module.exports = router;
